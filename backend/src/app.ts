6eb7e7a8459f40a4b10c24e1bdf23070ca323df2
mones <mones.cse@gmail.com>
2025-02-04 11:01:27 +0600

    backend port 5001, frontend port 3000, added tailwind in frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,7 +31,7 @@ app.use(
   }
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
